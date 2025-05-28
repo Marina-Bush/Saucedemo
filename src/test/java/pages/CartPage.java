29b8651299671e9c86f72a79047fb6f1413b7452
Saucedemo <ivoobraznaa@gmail.com>
2025-05-28 20:55:43 +0300

    tests

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -8,12 +8,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CartPage extends BasePage {
+    private static final By CART_ITEM = By.cssSelector(".cart_item");
+
     public CartPage(WebDriver driver) {
         super(driver);
     }
 
     public ArrayList<String> getProductsNames() {
-        List<WebElement> allProductNames = driver.findElements(By.cssSelector(".cart_item"));
+        List<WebElement> allProductNames = driver.findElements(CART_ITEM);
         ArrayList<String> names = new ArrayList<>();
         for (WebElement product : allProductNames) {
             names.add(product.getText());
